b1e370e07e94ac82efda00290381f39590e41b57
Lucas Thurston <lucas@castironcoding.com>
2023-08-11 15:09:04 -0700

    Revert "add support for TYPO3 12. remove support for TYPO3 9"
    This reverts commit 630583e0261fac00c1bca028b6f6f04c09475d4a.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3') or die();
+defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['dbalAndAdodbExtraction']
